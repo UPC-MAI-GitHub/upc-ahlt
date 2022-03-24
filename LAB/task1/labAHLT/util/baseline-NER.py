76238819761aa3dd01341543a6d7700b3b7a7d5d
“Eric <ewalzthony@gmail.com>
2022-03-24 14:54:09 +0100

    uploading mising files

--- LAB/task1/labAHLT/util/baseline-NER.py
+++ LAB/task1/labAHLT/util/baseline-NER.py
@@ -1,5 +1,4 @@
 #! /usr/bin/python3
-
 import sys
 from os import listdir,system
 import re
@@ -8,16 +7,21 @@ from xml.dom.minidom import parse
 from nltk.tokenize import word_tokenize
 
 import evaluator
+import pandas as pd 
+CHEAT_PATH = "/Users/Eric/Documents/Uni/Msc/Courses/Sem2/AHLT/LAB/task1/labAHLT/data/drugs.csv"
+df = pd.read_csv(CHEAT_PATH)
+dd = dict(zip(df.comp.values,df.type.values))
 
+## adding a specific path 
+PATH = "/Users/Eric/Documents/uni/msc/courses/Sem2/AHLT/LAB/task1/labAHLT"
 ## dictionary containig information from external knowledge resources
 ## WARNING: You may need to adjust the path to the resource files
 external = {}
-
-with open("labAHLT/resources/HSDB.txt") as h :
+with open(PATH + "/resources/HSDB.txt") as h :
     for x in h.readlines() :
         external[x.strip().lower()] = "drug"
 
-with open("labAHLT/resources/DrugBank.txt") as h :
+with open(PATH + "/resources/DrugBank.txt") as h :
     for x in h.readlines() :
         (n,t) = x.strip().lower().split("|")
         external[n] = t
@@ -35,33 +39,93 @@ def tokenize(txt):
         offset += len(t)
     return tks
 
+## check if the name contains special symbols 
+def check_chemical_name(some_text:str) -> str:
+    """Function which checks whether a text contains"""
+
 ## -----------------------------------------------
 ## -- check if a token is a drug part, and of which type
-
-suffixes = ['azole', 'idine', 'amine', 'mycin','amol','anil','anol','ol','zole','llin']
-n_suffixes = ['MHD', 'PCP', 'AOM', 'CPA', 'PTX', 'TAM', 'EGF', 'TML', 'VMA','RR', 'GL']
-#
-# Read the Resources in order to improve the model.
-# Lines to run:
-# Devel: 
-# python3 labAHLT/util/baseline-NER.py labAHLT/data/Devel/ result.out 
-# Train: 
-# python3 labAHLT/util/baseline-NER.py labAHLT/data/train/ result.out
+## --------------------- ADDED STUFF
+greek_prefix = ["hemi","mono",
+                "di","tri",
+                "tetra","penta",
+                "hexa","hepta",
+                "octa","nona",
+                "deca"]
+## Metallic Names 
+metal_names = ["cuprous","cupric",
+               "ferrous","ferric",
+               "mercurous","mercuric",
+               "stannous","stannic"]
+## Non-metal suffixes 
+non_metal_names = ["ide"]
+## Others
+oxyanion = ['ite','ate']
+## Bonds 
+bonds = ["ene",'yne']
+## functional groups 
+func_groups = [
+               "carboxy","carbamoyl",
+               "chloroformyl","hydroxy",
+               "formyl","oxo","alkyl",
+               "alkoxy","epoxy","halo",
+               "amine","cyano","nitro","nitroso",
+               "azo","sulpho","alkyl thio","mercapto"
+               ]
+## functional group suffixes 
+func_g_suffix = [
+                "oic","amide","oyl","chloride","acid",
+                "ol","al","one","carboxylate","amine","nitrile",
+                "sulphonic","thiol"
+                ]
+## other names 
+other_names2 = ["meth","eth","methyl","ethyl","yl"]
+org_prefix = ["meth","eth","prop",
+                  "but","pent","hex",
+                  "hept","oct","non","dec"]
+## bring them all together 
+tmp_list = [greek_prefix,
+            metal_names,non_metal_names,
+            oxyanion,
+            bonds,
+            func_groups,
+            func_g_suffix,other_names2,org_prefix]
+## merging & making into a set
+full_list = set(sum(tmp_list, []))
+## --------------------- END OF ADDED STUFF
+suffixes = ['azole', 'idine', 'amine', 'mycin']
 
 def classify_token(txt):
-
+    ## set up the counter 
+    cnt = 0
+    for something in full_list: 
+        if something in txt: 
+            cnt+1
+    ## other rules 
+    starts_with_digit = txt[:1].isdigit()
+    ## contains numbers?
+    contains_number = any(char.isdigit() for char in txt)
+    ## does it contain hyphens?
+    contain_hyphen = ("-" in txt)
+    ## contains parenthesis
+    contain_parenthesis = ("(" in txt) and (")" in txt)
+    ## does it contain a comma?
+    contain_comma = ("," in txt)
+    ## final values 
+    val = (starts_with_digit+contains_number+contain_hyphen+contain_parenthesis+contain_comma)
+    ## cheat a little bit 
+    #if txt in dd: return dd[txt]
    # WARNING: This function must be extended with 
    #          more and better rules
 
-   if txt.lower() in external : return external[txt.lower()]
-   # brands 
-   elif txt.isupper() : return "brand"
-   # drugs
-   elif txt[-5:] in suffixes : return "drug"
-   # drug_n
-   elif txt in n_suffixes: return "drug_n"
-   # groups
-   else : return "NONE"
+    if txt.lower() in external : return external[txt.lower()]
+    elif txt.isupper() : return "brand"
+    elif txt[-5:] in suffixes : return "drug"
+    elif contains_number and contain_hyphen and contain_parenthesis:
+        return "drug"
+    elif val >= 3 and cnt>=1: 
+        return "drug"
+    else : return "NONE"
 
    
 
